0b70bfafdd4a13d9edfd01645606916d27aaf462
Wo0zZ1 <Mper06@mail.ru>
2025-11-02 19:50:35 +0300

    feat: add kind-based Kubernetes cluster for CI/CD

---
+++ update-shared.bat
@@ -0,0 +1,34 @@
+@echo off
+echo ====================================
+echo Updating shared package in all services
+echo ====================================
+
+echo.
+echo [1/4] Updating api-gateway...
+cd api-gateway
+call yarn upgrade @wo0zz1/url-shortener-shared --latest
+cd ..
+
+echo.
+echo [2/4] Updating user-service...
+cd user-service
+call yarn upgrade @wo0zz1/url-shortener-shared --latest
+cd ..
+
+echo.
+echo [3/4] Updating auth-service...
+cd auth-service
+call yarn upgrade @wo0zz1/url-shortener-shared --latest
+cd ..
+
+echo.
+echo [4/4] Updating link-service...
+cd link-service
+call yarn upgrade @wo0zz1/url-shortener-shared --latest
+cd ..
+
+echo.
+echo ====================================
+echo Done! Now restart containers:
+echo docker-compose -f docker-compose.dev.yml restart
+echo ====================================
